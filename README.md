40bae8b2bad59aa7766935c81e39934c8c6ff3d3
don-rukshan <md.rukshan@gmail.com>
2020-03-22 20:24:23 +0530

    Phase 3: Creating the model , training

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # python-chatbot
+
 conda create -n chatbot python=3.6
 
 activate chatbot
@@ -10,3 +11,7 @@ pip install tensorflow (pip install tensorflow==1.13.2)
 
 tensorflow 2.0 or upwards don't have some stuff
 
+On Phase 3: when creating the model: running shows an error
+
+therefore open the python shell and download the below
+nltk.download('punkt')
--- main.py
+++ main.py
@@ -22,13 +22,13 @@ for intent in data["intents"]:
     for pattern in intent["patterns"]:
         wrds = nltk.word_tokenize(pattern)
         words.extend(wrds)
-        docs_x.append(pattern)
+        docs_x.append(wrds)
         docs_y.append(intent["tag"])
 
     if intent["tag"] not in labels:
         labels.append(intent["tag"])
 
-words = [stemmer.stem(w.lower()) for w in words]
+words = [stemmer.stem(w.lower()) for w in words if w != "?"]
 words = sorted(list(set(words)))
 
 labels = sorted(labels)
@@ -57,3 +57,16 @@ for x, doc in enumerate(docs_x):
 
 training = numpy.array(training)
 output = numpy.array(output)
+
+tensorflow.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+model.save("model.tflearn")
